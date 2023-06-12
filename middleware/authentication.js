d857babf3d4818196eeeb6e3e7f87853ce33c307
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-06-13 03:49:58 +0530

    minor changes

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,22 +1,33 @@
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
-const {UnauthenticatedError} = require('../errors')
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const asyncHandler = require("express-async-handler");
 
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-const auth = (req, res, next) => {
-    const authHeader = req.headers.authorization
-    if (!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new UnauthenticatedError('Authentication invalid')
-    }
-    const token = authHeader.split(' ')[1]
-    try{
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = {userId:payload.userId, name:payload.name}
-        next()
-    }
-    catch(err){
-        throw new UnauthenticatedError('Authentication invalid')
-    }
-}
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-module.exports = auth
+      //decodes token id
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = { protect };
