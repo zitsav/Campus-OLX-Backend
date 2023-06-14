4386b4717e0db2f40c427ffbc8ee7d7d6930d962
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-06-15 01:37:19 +0530

    minor changes (TODO)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { protect } = require("../middleware/authentication");
 const { allUsers } = require("../controllers/searchController");
+const { updateUser, getCurrentUserProfile } = require("../controllers/userController");
 const {authUser,logoutUser} = require("../controllers/sessionController");
 const {
   registerUser,
@@ -8,17 +9,26 @@ const {
   resetPassword,
   forgotPassword,
   deleteUser,
-} = require("../controllers/userController");
+} = require("../controllers/authController");
 
 const router = express.Router();
 
+// @TODO move these routes to authRoutes
 router.route("/").post(registerUser);
 router.route("/verify").post(verifyCode);
-router.post("/login", authUser);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/reset").post(resetPassword);
-router.route("/").get(protect, allUsers);
 router.delete("/:id",protect, deleteUser);
+
+// @TODO move these routes to sessionRoutes
 router.post("/logout",protect,logoutUser);
+router.post("/login", authUser);
+
+
+// @TODO these ones stay here
+router.put('/updateProfile/:id', protect, updateUser);
+router.get("/profile",protect,getCurrentUserProfile);
+router.route("/").get(protect, allUsers);
+
 
 module.exports = router;
