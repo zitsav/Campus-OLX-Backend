78fafdea1bcb013013fcbf0c2bb7dfc95a7937b9
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-06-14 17:33:23 +0530

    logout user added (Frontend)

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,18 +4,23 @@ const User = require("../models/User");
 // @desc    Get user profile
 
 const allUsers = asyncHandler(async (req, res) => {
+  try {
     const keyword = req.query.search
-    ? {
-        $or: [
-          { firstName: { $regex: req.query.search, $options: "i" } },
-          { lastName: { $regex: req.query.search, $options: "i" } },
-          { enrollmentNo: { $regex: req.query.search, $options: "i" } },
-        ],
-      }
-    : {};
-  
+      ? {
+          $or: [
+            { firstName: { $regex: req.query.search, $options: "i" } },
+            { lastName: { $regex: req.query.search, $options: "i" } },
+            { enrollmentNo: { $regex: req.query.search, $options: "i" } },
+          ],
+        }
+      : {};
+
     const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
     res.send(users);
-  })
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
 
 module.exports = { allUsers };
--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -5,37 +5,66 @@ const generateToken = require("../config/generateToken");
 // @desc    Login user
 
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-  
-    const user = await User.findOne({ email });
-  
-    if (!user) {
-      res.status(401);
-      throw new Error("Invalid email or password");
-    }
-  
-    if (user.registrationCode || user.registrationCodeExpiration || user.isVerified == false) {
-      res.status(401);
-      throw new Error("Please complete the registration process and verify your email");
-    }
-  
-    if (!(await user.comparePassword(password))) {
-      res.status(401);
-      throw new Error("Invalid email or password");
-    }
-  
-    res.json({
-      _id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      enrollmentNo: user.enrollmentNo,
-      semester: user.semester,
-      branch: user.branch,
-      contact: user.contact,
-      upiId: user.upiId,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  });
+  const { email, password } = req.body;
 
-  module.exports = { authUser };
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
+  if (
+    user.registrationCode ||
+    user.registrationCodeExpiration ||
+    user.isVerified == false
+  ) {
+    res.status(401);
+    throw new Error(
+      "Please complete the registration process and verify your email"
+    );
+  }
+
+  if (!(await user.comparePassword(password))) {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+  // res.header('Authorization', generateToken(user._id));
+  res.json({
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    enrollmentNo: user.enrollmentNo,
+    semester: user.semester,
+    branch: user.branch,
+    contact: user.contact,
+    upiId: user.upiId,
+    email: user.email,
+    token: generateToken(user._id),
+  });
+});
+
+// @desc logout user
+
+const logoutUser = async (req, res) => {
+  try {
+    const { user } = req;
+    if (!user) {
+      return res.status(401).json({ success: false, message: "Unauthorized access!" });
+    }
+
+    // @TODO implement delete token from db
+
+    //delete cookie from frontend
+    res.clearCookie("token");
+    res.status(200).json({ success: true, message: "Logout successful" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+module.exports = logoutUser;
+
+  
+
+module.exports = { authUser, logoutUser };
