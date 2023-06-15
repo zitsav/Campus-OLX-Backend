6fc4f5a9dbb069ac03cb57c11871bb76c0b86c61
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-06-15 18:18:49 +0530

    modified search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -15,7 +15,7 @@ const allUsers = asyncHandler(async (req, res) => {
         }
       : {};
 
-    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+      const users = await User.find(keyword, "-password -email").find({ _id: { $ne: req.user._id } });
     res.send(users);
   } catch (error) {
     res.status(500).json({ success: false, message: "Server error" });
