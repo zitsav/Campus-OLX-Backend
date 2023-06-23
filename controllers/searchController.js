f2232f7b314e4707a3fb6b40be0a08717a5f6163
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2023-06-23 14:54:00 +0530

    search byt product (optional)

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
+const Product = require("../models/Product");
 
 // @desc    Get other user profiles
 
@@ -22,5 +23,23 @@ const allUsers = asyncHandler(async (req, res) => {
   }
 });
 
+const allProducts = asyncHandler(async (req, res) => {
+  try {
+    const keyword = req.query.search
+      ? {
+          $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { category: { $regex: req.query.search, $options: "i" } },
+          ],
+        }
+      : {};
 
-module.exports = { allUsers };
+    const products = await Product.find(keyword);
+    res.send(products);
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+
+module.exports = { allUsers, allProducts };
